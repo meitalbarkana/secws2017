63bc657665606f5dfbf2bc5b6b416a9faedc9eee
meitalbarkana <chipimeital@gmail.com>
2018-02-28 18:48:22 +0200

    started working on combining 3 services

--- part4/firewall/proxy_server.py
+++ part4/firewall/proxy_server.py
@@ -1,4 +1,4 @@
-import socket, sys
+import socket, sys, select, Queue
 from httplib import HTTPResponse
 from StringIO import StringIO
 
@@ -50,41 +50,69 @@ def is_valid_content_length(all_data):
 	return True
 
 
+def is_valid_file(file_as_str):
+	"""
+	Gets FTP data, returns:
+			1. False if it's an .exe file
+			2. True otherwise.
+	NOTE: USE THIS ONLY ON FTP DATA (files)!
+	"""
+
+
 def start():
 	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#Initiate an INET, STREAMing socket
-		sock.bind((LOCALHOST, HTTP_LISTENING_PORT))					#Bind it to a our host and (well-known) HTTP_LISTENING_PORT port
-		sock.listen(MAX_CONN)										#Start listening
+		listening_ports = [HTTP_LISTENING_PORT, FTP_LISTENING_PORT_1, FTP_LISTENING_PORT_2]
+		#Initiate 3 INET, STREAMing sockets (for listening):
+		server_sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in xrange(3)]
+ 
+ 		for i in xrange(3):
+			server_sockets[i].setblocking(0)
+			server_sockets[i].bind((LOCALHOST, listening_ports[i]))				#Bind it to a our host and (well-known) relevant port
+			server_sockets[i].listen(MAX_CONN)									#Start listening
+
 	except Exception, e:
-		print("Error: unable to initialize listening socket")
+		print("Error: unable to initialize listening sockets")
 		print(e)
 		sys.exit(2)
 
-	while True:
-		try:
-			#print ("In while loop")
-			#sock.setblocking(0)
-			conn, addr = sock.accept()
-			data = conn.recv(MAX_BUFFER_SIZE)
-			if not data:
-				print("No data was received, moving on to next client")
-			else:
-				print("************************************************************************")
-				print("****************************Data is:****************************")
-				print(data)
-				print("****************************************************************")
-				#print("*********Does this data contain valid content length?**********")
-				print("*********************Data as dictionary is:*********************")
-				print(is_valid_content_length(data))
-				print("************************************************************************")
-				#start_new_thread(conn_string, (conn, data, addr))
-		except Exception, e:
-			#if (e == KeyboardInterrupt):
-			#	print("User requested to quit, exiting.")
-			#	sys.exit(1)
-			print(e)
-			pass
-			
+	output_sockets = []
+	input_sockets = server_sockets
+	message_queues = {}
+
+	while input_sockets:
+		print ("In while loop")
+
+		ready_to_read_sockets, ready_to_write_sockets, in_error_sockets = \
+               select.select(input_sockets, output_sockets, input_sockets)
+
+        for sock in ready_to_read_sockets:
+        	if sock in server_sockets:
+				try:
+					connection, client_address = http_sock.accept()
+					connection.setblocking(0)
+					input_sockets.append(connection)
+					"""FROM HERE ITS FOR THE HTTP:"""
+					data = connection.recv(MAX_BUFFER_SIZE)
+					if not data:
+						print("No data was received, moving on.")
+					else:
+						print("************************************************************************")
+						print("****************************HTTP received data is:****************************")
+						print(data)
+						print("****************************************************************")
+						print("*********Does this data contain valid content length?**********")
+						print(is_valid_content_length(data))
+						print("************************************************************************")
+					"""TILL HERE"""	
+				except Exception, e:
+					print(e)
+					pass
+			elif sock == server_sockets[1]:	#FTP_LISTENING_PORT_1
+				#TODO::
+			else:	#FTP_LISTENING_PORT_2
+				#TODO::
+
+	#TODO:: change next line accordingly:	
 	sock.close()
 
 
